80c9f4edf0d2026e1068135036b224b50c836480
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-11-02 10:13:07 +0100

    lab04

---
+++ Lab04/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿double QuadraticEquation(double x, double a, double b, double c)
+{
+    return 0.0;
+}
+
+
+void Swap(double a, double b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+double x = 10;
+double y = 20;
+Swap(x, y);
+Console.WriteLine($"x = {x}, y = {y}");
+
+
+double Multiply(double a, double b)
+{
+    return a * b;
+}
+
+void PrintDayOfTheWeek(DayOfTheWeek day)
+{
+    Console.WriteLine(day);
+}
+PrintDayOfTheWeek(DayOfTheWeek.Monday);
+
+
+string itemName = "eggplant";
+DateTime date = DateTime.Now;
+decimal price = 1.99m;
+Unit unit = Unit.item;
+Console.WriteLine($"On {date.Year}, the price of {itemName} was {price} per {unit}.");
+
+
+bool CheckTheAge(int age)
+{
+    if (age < 0)
+        throw new ArgumentOutOfRangeException("Age can not be a negative number!");
+    if (age >= 18)
+        return true;
+    else
+        return false;
+}
+
+enum DayOfTheWeek
+{
+    Monday
+}
+
+
+enum Unit { item, kilogram, gram, dozen };
