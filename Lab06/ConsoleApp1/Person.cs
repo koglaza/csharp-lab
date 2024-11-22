c9e49d42db30164672f6879ec7cbbb0eea41688f
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-11-22 16:02:16 +0100

    lab06

---
+++ Lab06/ConsoleApp1/Person.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    // class declaration
+    public class Person
+    {
+        public readonly static Person NewlyBornChild = new Person("", "") { BirthDate = DateTime.Today };
+        // constructors
+        public Person()
+        {
+
+        }
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public int? Height { get; set; }
+
+
+        // method
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Now.Year - this.BirthDate.Year;
+        }
+
+        public bool IsAdult()
+        {
+            return this.GetAge() > 18;
+        }
+    }
+}
---
+++ Lab06/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person() { FirstName = "John", LastName = "Doe" };
+            Person p2 = new Person() { FirstName = "Jane", LastName = "Doe", BirthDate = new DateTime(1999, 5, 11) };
+            Person p3 = new Person() { FirstName = "Jill", LastName = "Doe", BirthDate = new DateTime(2001, 2, 4), EmailAddress = "jill@gmail.com" };
+            Person child = Person.NewlyBornChild;
+            child.FirstName = "Brian";
+            child.LastName = "Doe";
+
+            Person p4 = null;
+            if (p4 is null)
+            {
+                Console.WriteLine("Person is null");
+            }
+            else
+            {
+                Console.WriteLine($"Name = {p4.GetFullName()}");
+            }
+            Console.WriteLine(p4 is null ? "Person is null" : $"Name = {p4.GetFullName()}");
+            Console.WriteLine((p4 ?? new Person("N", "N")).GetFullName());
+        }
+    }
+}
