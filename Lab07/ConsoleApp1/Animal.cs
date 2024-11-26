1dc4c2ec0e3e58fc7b1399bcb8ea72e684bd207b
konrad.oglaza <konrad.oglaza@schibsted.com>
2024-11-26 14:37:43 +0100

    lab07

---
+++ Lab07/ConsoleApp1/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Animal
+    {
+        public abstract string Sound { get; }
+        public virtual void Move()
+        {
+            Console.WriteLine("Moving...");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override string Sound => "Meow";
+        public override void Move()
+        {
+            Console.WriteLine("Walking like a cat...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override string Sound => "Woof";
+
+        public override void Move()
+        {
+            Console.WriteLine("Running like a dog...");
+        }
+    }
+
+}
---
+++ Lab07/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[] { new Cat(), new Dog() };
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"The {animal.GetType().Name} goes {animal.Sound}");
+                animal.Move();
+            }
+        }
+    }
+}
